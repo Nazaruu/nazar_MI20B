faac9ac9aedfbf415ff01d53dd710d38169b18bd
Nazaruu <ruhimatnazar@gmail.com>
2021-09-22 10:52:40 +0700

    Create Untitled-1.php

---
+++ Untitled-1.php
@@ -0,0 +1,29 @@
+<?php
+$nama = "Nazar Ruhimat";
+$Kelas = "MI20B";
+$Alamat = "Cisaga Jawabarat";
+$nohp = "085790601744";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WEEK 1 - Nazaru</title>
+</head>
+<body>
+    <?php
+        echo "Biodata";
+        echo "<br>"; //Untuk memberikan spasi
+        echo "<br>"; 
+        echo "Nama : ".$nama;
+        echo "<br>";
+        echo "Kelas : ".$Kelas;
+        echo "<br>";
+        echo "Alamat : ".$Alamat;
+        echo "<br>";
+        echo "No.Hp : ".$nohp;
+        echo "<br><br>";
+        echo "Tidak tahu Maka bertanya"
+       
+    ?>
+</body>
+</html>
